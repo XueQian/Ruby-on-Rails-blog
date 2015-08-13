0ddbfdbebd76cb0207a32f261e0a00fa03439238
xueqian <xueqian0321@outlook.com>
2015-08-13 17:26:32 +0800

    add an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,25 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    @article.save
+    redirect_to @article
+  end
+
+  # def create
+  #   @article = Article.new(article_params)
+  #
+  #   @article.save
+  #   redirect_to @article
+  # end
+  #
+  # private
+  # def article_params
+  #   params.require(:article).permit(:title, :text)
+  # end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
